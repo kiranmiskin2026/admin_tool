ce25272a20d03513f6055f1225f0d84b945172d2
KIRAN-MISKIN <kiranm121998@gmail.com>
2026-02-06 13:03:48 +0530

    changed jenkins file 4

--- db.js
+++ db.js
@@ -5,4 +5,4 @@ mongoose.connect('mongodb://localhost:27017/admin_tool')
   .catch(err => console.error('MongoDB connection error:', err));
 
 module.exports = mongoose.connection;
-//MongoDB changed
+//MongoDB changed 
